9b25acdd38306f334c627921a56309a257617580
Phoenix <akajyu9@gmail.com>
2019-11-21 10:55:34 +0100

    original

---
+++ YinA/src/Yin1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Yin1 {
+
+	public static void main(String[] args) {
+		int n = 7776777, c=0;
+		while (n!=1) {
+			c++;
+			if ((n%2)==0) {
+				n=n/2; 
+			} else n=(n*3)+1;
+			System.out.println(n);
+		}
+		System.out.println(c);
+
+	}
+
+}
